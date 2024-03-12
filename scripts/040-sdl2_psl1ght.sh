c930464725e4a7bcfef05dbe06389f4a3afc5f2d
Humberto Dias <hldtux@gmail.com>
2024-03-12 02:31:06 -0300

    psl1ght-2.30.1

--- scripts/040-sdl2_psl1ght.sh
+++ scripts/040-sdl2_psl1ght.sh
@@ -3,7 +3,9 @@
 
 ## Download the source code.
 # SDL2
-wget https://github.com/humbertodias/SDL2_PSL1GHT/tarball/master -O sdl2_psl1ght.tar.gz
+#wget https://github.com/humbertodias/SDL2_PSL1GHT/tarball/master -O sdl2_psl1ght.tar.gz
+#wget https://github.com/humbertodias/psl1ght-2.0.3/tarball/psl1ght-2.0.3 -O sdl2_psl1ght.tar.gz
+wget https://github.com/humbertodias/sdl2_psl1ght_/tarball/psl1ght-2.30.1 -O sdl2_psl1ght.tar.gz
 
 ## Unpack the source code.
 rm -Rf sdl2_psl1ght && mkdir sdl2_psl1ght && tar --strip-components=1 --directory=sdl2_psl1ght -xvzf sdl2_psl1ght.tar.gz
