8cab78f06419a5dc73e0ad1f45f92a19d60d488f
Humberto Dias <hldtux@gmail.com>
2026-09-07 17:57:44 -0300

    Fix host libavif leak in 040-sdl2_psl1ght_libs.sh macOS cross-builds

--- scripts/040-sdl2_psl1ght_libs.sh
+++ scripts/040-sdl2_psl1ght_libs.sh
@@ -22,5 +22,11 @@ cd sdl2_psl1ght_libs
 ## Preload config.guess and config.sub
 cp ../../archives/config.guess ../../archives/config.sub archives/
 
+## Host Homebrew libavif/jpeg-xl must not leak into the PPU SDL2_image link.
+cat ../../patches/sdl2_psl1ght_libs-1.patch | patch -p1
+
+export PKG_CONFIG_LIBDIR="${PS3DEV}/portlibs/ppu/lib/pkgconfig"
+export PKG_CONFIG_PATH="${PS3DEV}/portlibs/ppu/lib/pkgconfig"
+
 ## Compile and install.
 ./make_SDL_Libs.sh
