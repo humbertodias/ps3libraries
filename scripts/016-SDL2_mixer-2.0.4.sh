9646c0bd4c0b7438a7ed91c95d9c181c22299f38
Humberto Dias <hldtux@gmail.com>
2026-09-07 17:00:31 -0300

    Drop the official SDL 1.2/2/3 stack from this branch.
    Those libraries live on sdl_1_2_3; restore the PSL1GHT SDL snapshots here.

--- scripts/016-SDL2_mixer-2.0.4.sh
+++ scripts/016-SDL2_mixer-2.0.4.sh
@@ -1,37 +1,35 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# SDL_mixer-1.2.11.sh
-SDL_MIXER="SDL_mixer-1.2.11"
+SDL2_MIXER="SDL2_mixer-2.0.4"
 
 ## Source util functions
 source ../utils/utils.sh
 
 ## Download the source code.
-../download.sh ${SDL_MIXER}.tar.gz
+../download.sh ${SDL2_MIXER}.tar.gz
 
 ## Fetch config.guess and config.sub, falling back to copies if Savannah is unavailable
 ../config/get-config-scripts.sh
 
 ## Unpack the source code.
-rm -Rf ${SDL_MIXER}
-echo "Unpacking ${SDL_MIXER}"
-extract ../archives/${SDL_MIXER}.tar.gz
-cd ${SDL_MIXER}
+rm -Rf ${SDL2_MIXER}
+echo "Unpacking ${SDL2_MIXER}"
+extract ../archives/${SDL2_MIXER}.tar.gz
+cd ${SDL2_MIXER}
 
 ## Replace config.guess and config.sub
-cp ../../archives/config.guess ../../archives/config.sub .
 cp ../../archives/config.guess ../../archives/config.sub build-scripts/
 
-## Patch the source code.
-cat ../../patches/${SDL_MIXER}.patch | patch -p1
+## Create the build directory.
+mkdir build-ppu && cd build-ppu
 
 ## Configure the build.
-CFLAGS="-I$PSL1GHT/ppu/include -I$PSL1GHT/ppu/include/SDL -I$PS3DEV/portlibs/ppu/include -I$PS3DEV/portlibs/ppu/include/SDL" \
+CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
-PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
+PKG_CONFIG_LIBDIR="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 LIBMIKMOD_CONFIG="$PS3DEV/portlibs/ppu/bin/libmikmod-config" \
-./configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" \
+../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" \
     --disable-sdltest \
     --with-sdl-exec-prefix="$PS3DEV/portlibs/ppu" \
     --disable-shared \
@@ -39,7 +37,8 @@ LIBMIKMOD_CONFIG="$PS3DEV/portlibs/ppu/bin/libmikmod-config" \
     --disable-music-ogg-shared \
     --disable-music-mp3 \
     --disable-music-flac \
-    --enable-static
+    --disable-music-mod-modplug \
+    --enable-music-mod-mikmod
 
 ## Compile and install.
 jobs=$(nproc 2>/dev/null || sysctl -n hw.ncpu)
